66696a4aa713c7d5da22a8dbc83b7e460058e25d
je0489 <je0489@naver.com>
2021-05-26 02:02:18 +0900

    2nd report

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 
-const PORT = 4000;
-
 const app = express();
-const handleListening = () => console.log( `Server listening on port https://localhost:${PORT}` );
-app.listen( PORT, handleListening );
+
+app.get( "/", ( req, res ) => res.send( "Here is HomePage" ) );
+app.get( "/about", ( req, res ) => res.send( "Here is AboutPage" ) );
+app.get( "/contact", ( req, res ) => res.send( "Here is ContactPage" ) );
+app.get( "/login", ( req, res ) => res.send( "Here is LoginPage" ) );
+
+app.listen( 4000 );
