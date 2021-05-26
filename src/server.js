2f4452e60452c319f7c855773cd70f9ea2b256c0
je0489 <je0489@naver.com>
2021-05-26 22:22:34 +0900

    #3.6 Middlewareds PART 2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 import express from "express";
 
 const app = express();
-const gossipMiddleware = (req, res, next) => {
-    console.log( `Someone is going to: ${req.url}` );
+const logger = (req, res, next) => {
+    console.log( `${req.method} ${req.url}` );
     next();
-}
+};
 
-app.get( "/", gossipMiddleware, ( req, res ) => res.send( "Here is HomePage" ) );
+const privateMiddleware = ( req, res, next ) => {
+    const url = req.url;
+    if( url === "/protected" ) {
+        return res.send( "<H1>Not Allowed</H1>");
+    }
+    
+    console.log( "Allowed, you may continue." );
+    next();
+};
+
+app.use( logger );
+app.use( privateMiddleware );
+
+app.get( "/", ( req, res ) => res.send( "I love middlewares" ) );
+app.get( "/protected", ( req, res ) => res.send( "Welcome to the private lounge" ) );
 
 app.listen( 4000 );
