2d3f0c610bf627332a04b2476bf4628776c1c8d7
je0489 <je0489@naver.com>
2021-05-26 23:50:58 +0900

    #3.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,23 +1,9 @@
 import express from "express";
+import mogran from "morgan";
 
 const app = express();
-const logger = (req, res, next) => {
-    console.log( `${req.method} ${req.url}` );
-    next();
-};
 
-const privateMiddleware = ( req, res, next ) => {
-    const url = req.url;
-    if( url === "/protected" ) {
-        return res.send( "<H1>Not Allowed</H1>");
-    }
-    
-    console.log( "Allowed, you may continue." );
-    next();
-};
-
-app.use( logger );
-app.use( privateMiddleware );
+app.use( mogran( "dev" ) );
 
 app.get( "/", ( req, res ) => res.send( "I love middlewares" ) );
 app.get( "/protected", ( req, res ) => res.send( "Welcome to the private lounge" ) );
