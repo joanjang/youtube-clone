33ef4c1e8fd9849dd86dc03a7c3a30b2c39ba8fb
je0489 <je0489@naver.com>
2021-05-27 22:44:29 +0900

    #4.1 Making Our Routers

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 import express from "express";
-import mogran from "morgan";
+import morgan from "morgan";
 
 const app = express();
+const logger = morgan("dev");
 
-app.use( mogran( "dev" ) );
+app.use( logger );
 
-app.get( "/", ( req, res ) => res.send( "I love middlewares" ) );
-app.get( "/protected", ( req, res ) => res.send( "Welcome to the private lounge" ) );
+const globalRouter = express.Router();
+const handleHome = ( req, res ) => res.send( "HOME" ); 
+globalRouter.get( "/", handleHome );
+
+const userRouter = express.Router();
+const handleEditUser = ( req, res ) => res.send( "Edit User" ); 
+userRouter.get( "/edit", handleEditUser );
+
+const videoRouter = express.Router();
+const handleWatchVideo = ( req, res ) => res.send( "Watch Video" ); 
+videoRouter.get( "/watch", handleWatchVideo );
+
+app.use( "/", globalRouter );
+app.use( "/users", userRouter );
+app.use( "/videos", videoRouter );
 
 app.listen( 4000 );
