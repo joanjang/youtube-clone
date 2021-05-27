7a96e7306b8c22220a112c9cd631c3a97df09af5
je0489 <je0489@naver.com>
2021-05-28 00:36:42 +0900

    #4.3 Exports

--- src/routers/globalRouters.js
+++ src/routers/globalRouters.js
@@ -1,11 +1,10 @@
 import express from "express";
+import { join } from "../controllers/userController";
+import { trending } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-const handleHome = ( req, res ) => res.send( "HOME" );
-const handleJoin = ( req, res ) => res.send( "JOIN" );
-
-globalRouter.get( "/", handleHome ); 
-globalRouter.get( "/join", handleJoin ); 
+globalRouter.get( "/", trending ); 
+globalRouter.get( "/join", join ); 
 
 export default globalRouter;
