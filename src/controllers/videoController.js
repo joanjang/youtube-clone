19c5fc92fcf861a03b757a39ec2e47b7b4dad717
je0489 <je0489@naver.com>
2021-05-30 02:14:18 +0900

    #4.7 URL Parameters Part 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,13 @@
 export const trending = ( req, res ) => res.send( "Home Page Videos" );
-export const watch = ( req, res ) => res.send( "Watch Video" ); 
-export const edit = ( req, res ) => res.send( "Edit Video" );
+export const see = ( req, res ) => {
+    return res.send( `Watch Video #${req.params.id}` ); 
+}
+export const edit = ( req, res ) => {
+    return res.send( `Edit Video #${req.params.id}` ); 
+}
+export const search = ( req, res ) => res.send( "Search Video" );
+export const upload = ( req, res ) => res.send( "Upload Video" );
+export const deleteVideo = ( req, res ) => {
+    return res.send( `Delete Video #${req.params.id}` ); 
+}
+
