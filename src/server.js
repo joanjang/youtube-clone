577dd5a434c249f21f7231dfa7c3bba53f7bb701
je0489 <je0489@naver.com>
2021-05-31 23:01:43 +0900

    #5.3 Extending Templates: Pug render

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import videoRouter from "./routers/videoRouters";
 const app = express();
 const logger = morgan("dev");
 
+app.set( "view engine", "pug") ;
+app.set( "views", process.cwd() + "/src/views" );
 app.use( logger );
 
 app.use( "/", globalRouter );
