d3a6191c21f5c7b6c023d4de13705f6a4dfb0a9b
je0489 <je0489@naver.com>
2021-06-04 21:04:32 +0900

    #6.7~10 Using the mongoDB and mongoose

---
+++ src/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+mongoose.connect( "mongodb://127.0.0.1:27017/wetube" , 
+                { useNewUrlParser: true, useUnifiedTopology: true } );
+
+const db = mongoose.connection;
+db.on( "error", ( error ) => console.log( "[] DB ERROR", error ) );
+db.once( "open", () => console.log( "Connected to DB" ) ); 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
+import "./db"
+import "./models/Video";
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouters";
 import userRouter from "./routers/userRouters";
 import videoRouter from "./routers/videoRouters";
+
+const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -11,10 +16,11 @@ app.set( "view engine", "pug") ;
 app.set( "views", process.cwd() + "/src/views" );
 app.use( logger );
 
-app.use(express.urlencoded( { extended: true }));
+app.use( express.urlencoded( { extended: true } ) );
 
 app.use( "/", globalRouter );
 app.use( "/users", userRouter );
 app.use( "/videos", videoRouter );
 
-app.listen( 4000 );
+app.listen( PORT, () =>
+    console.log( `[] Server listening on port http://localhost:${PORT}`) );
