d391f6983c97d2c80a2c0329244a65f44ff75db1
je0489 <je0489@naver.com>
2021-06-08 01:26:19 +0900

    #6.21~22 Edit Video PART 2, 3: chagned postEidt

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -21,13 +21,23 @@ export const getEdit = async ( req, res ) => {
     const { id } = req.params;
     const video = await Video.findById( id );
     if( !video ) {
-      return res.render( "404", {pageTitle: "Video not found." } );
+      return res.render( "404", { pageTitle: "Video not found." } );
     }
     return res.render( "edit", { pageTitle: `Editing: ${video.title}`, video } );
 }
-export const postEdit = ( req, res ) => {
+export const postEdit = async ( req, res ) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    if( await !Video.exists( { _id: id } ) ) {
+      return res.render( "404", { pageTitle: "Video not found" } );
+    }
+    await Video.findByIdAndUpdate( id, {
+      title, 
+      description, 
+      hashtags: hashtags
+        .split( "," )
+      .map( (word) => word.startsWith( "#" ) ? word : `#${word}` ) 
+    } );
     return res.redirect( `/videos/${id}`);
 }
 export const getUpload = ( req, res ) => {
