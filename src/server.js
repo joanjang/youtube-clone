4197f48633d53415f34d241a918e6b13ddd3232f
je0489 <je0489@naver.com>
2021-06-11 00:55:39 +0900

    #7.7~8 Sessions and Cookies

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouters";
@@ -11,8 +12,25 @@ const logger = morgan( "dev" );
 app.set( "view engine", "pug") ;
 app.set( "views", process.cwd() + "/src/views" );
 app.use( logger );
-
 app.use( express.urlencoded( { extended: true } ) );
+
+app.use( session ( {
+    secret: "HELLO",
+    resave: true,
+    saveUninitalized: true
+} ) );
+
+app.use( ( req, res, next ) => {
+    req.sessionStore.all( ( error, sessions ) => {
+        console.log( sessions );
+        next();
+    } );
+} );
+
+app.get("/add-one", ( req, res, next ) => {
+    req.sessiont.apple += 1;
+    return res.send( `${req.session.id} ${req.session.apple}`);
+} );
 
 app.use( "/", rootRouter );
 app.use( "/users", userRouter );
