cf4d5c6f9db4c39ff3cc3992f0849a02a5573dab
je0489 <je0489@naver.com>
2021-06-15 22:50:34 +0900

    #8.7 File Uploads PART 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -56,15 +56,15 @@ export const getEdit = ( req, res ) => res.render( "edit-profile", { pageTitle: 
 export const postEdit = async ( req, res ) => {
   const {
     session: {
-      user: { _id }
+      user: { _id, avatarUrl }
     },
     body: { name, email, username, location },
     file
   } = req;
-  console.log( file );
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
--- src/middlewares.js
+++ src/middlewares.js
@@ -3,7 +3,7 @@ import multer from "multer";
 export const localsMiddleware = ( req, res, next ) => {
     res.locals.loggedIn = Boolean( req.session.loggedIn );
     res.locals.siteName = "Wetube";
-    res.locals.loggedInUser = req.session.user;
+    res.locals.loggedInUser = req.session.user || {};
 
     next();
 };
