7f5eaf39baa43e5db90f54a043a96378c113420a
je0489 <je0489@naver.com>
2021-06-15 20:49:05 +0900

    #7.12 MongoStore

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,5 @@
 export const localsMiddleware = ( req, res, next ) => {
-    res.locals.loggedIn = Bool( req.session.loggedIn );
+    res.locals.loggedIn = Boolean( req.session.loggedIn );
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user;
     next();
