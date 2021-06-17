5c97ba76c70c0121368f7f4d3cfed4590ecd7359
je0489 <je0489@naver.com>
2021-06-18 01:54:21 +0900

    #9.5 MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,33 @@
-const path = require( "path" );
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: "./src/client/js/main.js",
-    mode: "development",
-    output: {
-        filename: "main.js",
-        path: path.resolve( __dirname, "assets", "js" )
-    },
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: [["@babel/preset-env", { targets: "defaults" }]],
-              },
-            },
+  entry: "./src/client/js/main.js",
+  mode: "development",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
-          {
-            test: /\.scss$/,
-            use: ["style-loader", "css-loader", "sass-loader"],
-          }
-        ]
-    }
-}
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
