84be2cd0962b7fee5655357629bf04c0228522bb
je0489 <je0489@naver.com>
2021-06-21 19:56:31 +0900

    #11.4~5 video duration and currentTime: 비디오 재생 시간 및 전체 시간

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,24 +1,45 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
-console.log( "video Player");
+let volumeVale = 0.5;
+video.volume = volumeVale;
 
-const handlePlayClick = (e) => {
+const formatTime = ( seconds ) => new Date( seconds * 1000 ).toISOString().substr( 11, 8 );
+const handlePlayClick = () => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
-
-const handleMute = (e) => {};
+const handleMute = () => {
+  if( video.muted )
+    video.muted = false;
+  else
+    video.muted = true;
+  
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeVale;
+};
+const handleVolmeChagne = (event) => {
+  const { target: { value } } = event;
+  if( video.muted ) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeVale = value;
+  video.volume = value;
+};
+const handleLoadedMetadata = () => totalTime.innerText = formatTime( Math.floor( video.duration ) );
+const handleTimeUpdate = () => currentTime.innerText = formatTime( Math.floor( video.currentTime ) );
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+volumeRange.addEventListener("input",handleVolmeChagne);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
